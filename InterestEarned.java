6401fc156fbdb3945d66de60fe3b167e154f8ee1
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-04 13:37:53 -0400

    Work For Interest assigment

--- InterestEarned.java
+++ InterestEarned.java
@@ -1,3 +1,31 @@
-public class InterestEarned {
-    
+import java.util.Scanner;
+public class InterestEarned 
+{
+    public static void main(String[] args)
+    {
+      Scanner keyboard= new Scanner(System.in);
+      double Principal;
+      double InterestRate;
+      double InterestCompound;
+      double rate;
+      double amount;
+      double Interest;
+      System.out.print("What is the Principal Amount: ");
+      Principal=keyboard.nextDouble();
+      System.out.print("What is the annual Interest rate: ");
+      InterestRate=keyboard.nextDouble();
+      System.out.print("The number of times interest is compounded during the year: ");
+      InterestCompound=keyboard.nextDouble();
+      rate=InterestRate/100.0;
+      amount=Principal*Math.pow(1+(rate/InterestCompound),InterestCompound);
+      Interest=amount-Principal;
+      System.out.println();
+      System.out.printf("Interest Rate: $%.2f\n",InterestRate);
+      System.out.printf("Times Compound: $%.0f\n", InterestCompound);
+      System.out.printf("Principal: $%.2f\n", Principal);
+      System.out.printf("Interest earned: $%.2f\n", Interest);
+      System.out.printf("Amount in savings: $%.2f\n", amount);
+
+
+    }
 }
